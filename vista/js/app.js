e3327a246bc84aeab28c89f8f9fc166435b639b8
Augusto BM <137666674+augusto-BM@users.noreply.github.com>
2024-07-07 13:40:05 -0500

    medio dia

--- vista/js/app.js
+++ vista/js/app.js
@@ -11,7 +11,7 @@ let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 let timeRunning = 3000;
-let timeAutoNext = 15000;
+let timeAutoNext = 10000;
 
 nextDom.onclick = function(){
     showSlider('next');    
